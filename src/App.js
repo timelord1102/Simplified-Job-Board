afff87e321cb39e6c8b8b35c1e3f0a10eec336d4
Ariel Montero <ariel.montmaj@gmail.com>
2024-02-18 00:32:09 -0500

    organized code and restructured and added many elements. Added CSS to site

--- src/App.js
+++ src/App.js
@@ -1,76 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { createJobArray } from './jobs.js';
+import mp3_file from './Audio/Cyber Grind (Ultrakill Soundtrack).mp3';
 
 
-let sponsorship = ""
-
-function JobFilter({job}) {
-  if (job.sponsorship === sponsorship || sponsorship === "") {
-    return (
-      <li key={job.id}>
-        <h3>{job.title}</h3>
-        <h4>{job.company}</h4>
-        <h5>{job.locations}</h5>
-        <h5>{job.sponsorship}</h5>
-      </li>
-    )
-  }
-  return null
-
-}
-
-function val() {
-  return document.getElementById("sponsorship").value;
-}
+let sponsorship = "All"
 
 function App() {
   const [jobs, filterJobs] = React.useState(
     basejobs
   )
-  const [sponsorship, setSponsorship] = React.useState("")
 
+  var audio = new Audio(mp3_file);
+  function togglePlay() {
+    var play = document.querySelector('.play');
+    if (play.innerHTML === '❚❚') {
+      audio.pause()
+      play.innerHTML = '▶';
+    } else {
+      audio.play();
+      play.innerHTML = '❚❚';
+    }
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>
+        <p className='play' onClick={togglePlay}>▶</p>
+        <p className='title'>
+          ECONOMY IS DEAD.
+          <br />
+          DEPRESSION IS FUEL.
+          <br />
+          JOBS ARE FULL.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <select id = "sponsorship">
-        <option value="">All</option>
-        <option value="Offers Sponsorship">Offers Sponsorship</option>
-        <option value="Does Not Offer Sponsorship">Does Not Offer Sponsorship</option>
-      </select>
-      
-      <button onClick={() => filterJobs(
-        basejobs.filter(j => j.sponsorship ===  val() || val() === "")
-      )}>Apply</button>
+      </h1>
+    <div className = "sponsorDrop">
+      <button className = "sponsorship">{sponsorship}</button>
+      <div className = "sponsorshipContent">
+        <p onClick={() => {
+          sponsorship = "All";
+          filterJobs(basejobs)}}>All</p>
+
+        <p onClick={() => {
+          sponsorship = "Offers Sponsorship"
+          filterJobs(basejobs.filter(j => j.sponsorship === "Offers Sponsorship" || sponsorship === ""))}}>Offers Sponsorship</p>
+      </div>
+    </div>
       {jobs.map(job => (
-        <li key={job.id}>
+        <li className='Jobs' key={job.id}>
+          <h2></h2>
           <h3>{job.title}</h3>
           <h4>{job.company}</h4>
-          <h5>{job.locations}</h5>
-          <h5>{job.sponsorship}</h5>
+          <h5>
+            <button className = 'apply' onClick={() => window.open(job.url, '_blank')}>APPLY</button>
+          </h5>
+          
         </li>
 
       ))}
 
     </div>
+    
   );
 }
 
 const basejobs = await createJobArray()
 console.log(basejobs)
+
 export default App;
